ef752c66daa382722ccac84e1f26969b5e8b952a
Enricozc <enrico.candido08@gmail.com>
2025-10-20 14:27:42 -0300

    Criando lista produtos e adicionar produtos em front-end

--- front-end/app.py
+++ front-end/app.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import requests
 
-BASE_URL = "http://127.0.0.1:8000" 
+BASE_URL = "http://127.0.0.1:8000"
 
+st.set_page_config(page_title="Gerenciador de Produtos", page_icon="🛒")
 st.title("🛒 Gerenciador de Produtos")
+
+# --- MENU ---
+menu = st.sidebar.selectbox("Menu", ["Listar Produtos", "Adicionar Produto"])
+
+# --- LISTAR PRODUTOS ---
+if menu == "Listar Produtos":
+    st.subheader("📦 Lista de Produtos")
+
+    response = requests.get(f"{BASE_URL}/produtos")
+
+    if response.status_code == 200:
+        produtos = response.json()["produtos"]
+
+        if produtos:
+            for p in produtos:
+                with st.container():
+                    st.markdown(f"### {p['nome']}")
+                    st.write(f"📝 Descrição: {p['descricao']}")
+                    st.write(f"💰 Preço: R$ {p['preco']:.2f}")
+                    st.write(f"📦 Estoque: {p['estoque']} unidades")
+                    st.write(f"🏷️ Categoria: {p['categoria']}")
+ 
